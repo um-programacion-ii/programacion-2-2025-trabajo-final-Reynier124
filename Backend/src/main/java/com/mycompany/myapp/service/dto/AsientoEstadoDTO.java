5111bd9a68c103039889b83716ae5ca917dfb950
Reynier124 <reynier1306@gmail.com>
2025-12-20 18:38:50 -0300

    Mapa de asientos creados

---
+++ Backend/src/main/java/com/mycompany/myapp/service/dto/AsientoEstadoDTO.java
@@ -0,0 +1,11 @@
+package com.mycompany.myapp.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AsientoEstadoDTO {
+    private int columna;
+    private String estado;
+}
